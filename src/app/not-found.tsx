dfc3f44d84b556333c72bc19552ae1caf497c24e
SuhaibGit <m.suhaib1906@gmail.com>
2024-12-08 23:46:07 +0500

    updated

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>Page Not Found </h2>
+      <p>COULDN'T FIND REQUEST</p>
+    </div>
+  )
+}
+
+export default NotFound
