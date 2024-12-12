2c322d1f6bca11549928ee8b2576c2d6e7fe7dbb
SuhaibGit <m.suhaib1906@gmail.com>
2024-12-12 11:31:58 +0500

    Responsive updated

--- src/app/components/esp.tsx
+++ src/app/components/esp.tsx
@@ -4,7 +4,7 @@ import { IoIosCheckmarkCircle } from "react-icons/io";
 
 const Esp = () => {
   return (
-    <div className="h-auto md:h-[444px] bg-[url('/assets/email2.jpg')] bg-cover bg-center flex flex-col justify-center items-center p-5 ">
+    <div className="h-auto md:h-[444px] bg-[url('/assets/email2.jpg')] bg-cover bg-center flex flex-col justify-center items-center p-5 text-[white] ">
      <h1 className='text-[23px] md:text-[32px] '>Join the club and get the benefits</h1> 
      <div className='mt-4 w-[95%] md:w-[490px] text-center'>
      <p className='text-[14px] md:text-[18px]'>Sign up for our newsletter and receive exclusive offers on new ranges, sales, pop up stores and more</p>
